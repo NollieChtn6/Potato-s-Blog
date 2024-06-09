e3f126e07ec2110e7bf398be2f91775bd00da795
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-09 20:53:37 +0200

    wrap App component into app context provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,21 @@ import Footer from './components/footer/Footer';
 import ScreenSize from './components/devComponent/ScreenSize';
 import Navigation from './components/nav/Navigation';
 
+import AppProviders from './contexts';
+
 function App() {
   return (
-    <div className="app-container h-screen w-screen flex flex-col bg-[url('/dragon-scales.svg')]">
-      <Header />
-      <body className="flex flex-row w-full h-full overflow-hidden">
-        <Navigation />
-        <Outlet />
-      </body>
-      <Footer />
-      <ScreenSize />
-    </div>
+    <AppProviders>
+      <div className="app-container h-screen w-screen flex flex-col bg-[url('/dragon-scales.svg')]">
+        <Header />
+        <body className="flex flex-row w-full h-full overflow-hidden">
+          <Navigation />
+          <Outlet />
+        </body>
+        <Footer />
+        <ScreenSize />
+      </div>
+    </AppProviders>
   );
 }
 
