47f3ccb567c6863f74dcdf729502f4eae4eaa67d
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-09 21:00:00 +0200

    pass props to CategoryCard component and update content accordinly

--- src/components/categoriesPage/categoryCard.tsx
+++ src/components/categoriesPage/categoryCard.tsx
@@ -2,19 +2,20 @@ import { NavLink } from 'react-router-dom';
 
 import { Button } from '../ui/button';
 
-function CategoryCard() {
+import { CategoryInterface } from '../../@types/dataTypes';
+
+interface CategoryProps {
+  category: CategoryInterface;
+}
+
+function CategoryCard({ category }: CategoryProps) {
   return (
     <div className="latest-article-container flex flex-col h-[250px] w-full lg:max-w-[400px] p-3 border-solid border-2 border-cadet-blue-400  mx-3 rounded-md shadow-lg shadow-cadet-blue-500/70 bg-cadet-blue-200 space-y-3">
       <div className="latest-article-header flex flex-col h-[35%] border-solid border-b-[1px] border-cadet-blue-300">
-        <p className="text-xl">Adventures</p>
+        <p className="text-xl">{category.name}</p>
       </div>
       <div className="latest-article-body flex h-[45%]">
-        <p className="text-md">
-          Ever wondered what it’s like to see the world through the eyes of a
-          cat? Join Patate as she navigates through daring feats of courage,
-          from high-flying leaps off the couch to the treacherous tree climbing
-          escapades.
-        </p>
+        <p className="text-md">{category.description}</p>
       </div>
       <div className="latest-article-btn-container flex h-[20%] align-middle justify-end">
         <NavLink to="/articles/">
