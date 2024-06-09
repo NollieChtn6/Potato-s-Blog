30de02721b1b4e6859f670bdea1f5e39a816c173
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-09 16:46:33 +0200

    create footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer-container flex h-16 mt-auto items-center justify-center p-6 bg-cadet-blue-400/30">
+      <p className="text-pewter-900 font-serif">Potato&rsquo;s Blog • 2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
