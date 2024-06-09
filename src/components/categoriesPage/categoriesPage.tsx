14765ea809cfe52e40e2ab4e9771fa6f598c0f1d
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-09 20:59:19 +0200

    use categoriesContext to display categoriess on categories page

--- src/components/categoriesPage/categoriesPage.tsx
+++ src/components/categoriesPage/categoriesPage.tsx
@@ -1,6 +1,10 @@
 import CategoryCard from './categoryCard';
+import { useCategories } from '../../contexts/categoriesContext';
 
 function Categories() {
+  const { categories } = useCategories();
+  const categoriesList = categories;
+
   return (
     <main className="articles-page-container flex flex-col overflow-y-hidden h-full w-full text-pewter-900 p-3 space-y-6">
       <div className="breadcrumbs-container flex bg-slate-400 w-full h-[30px] items-center">
@@ -10,10 +14,9 @@ function Categories() {
         <h2 className="text-4xl font-serif text-pewter-900">Categories</h2>
       </div>
       <div className="categories-page-content flex flex-col w-full overflow-y-scroll space-y-6 lg:flex-row lg:flex-wrap">
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
+        {categoriesList.map((category) => (
+          <CategoryCard key={category.id} category={category} />
+        ))}
       </div>
     </main>
   );
