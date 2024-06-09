52de5a8359fadbc68183dfd6cba30dc3363acbf0
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-09 22:37:51 +0200

    create route to /categories/:slug

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,7 +4,7 @@ import App from '../App';
 import HomePage from '../components/homepage/HomePage';
 import About from '../components/about/About';
 import Categories from '../components/categoriesPage/categoriesPage';
-
+import CategoryPage from '../components/category/CategoryPage';
 import ArticlesPage from '../components/articlesPage/articlesPage';
 
 const router = createBrowserRouter([
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       { path: '/about', element: <About /> },
       { path: '/categories', element: <Categories /> },
+      { path: '/categories/:slug', element: <CategoryPage /> },
       { path: '/articles', element: <ArticlesPage /> },
     ],
   },
