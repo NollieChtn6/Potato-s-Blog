f6cccfacb129f93206bdf1d57af335bdc0ac7bc8
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-10 10:49:06 +0200

    display article related data dynamically and add navlink to related category

--- src/components/homepage/LatestArticleCard.tsx
+++ src/components/homepage/LatestArticleCard.tsx
@@ -13,13 +13,15 @@ function LatestArticleCard({ article }: ArticleInterface) {
     <div className="latest-article-container flex flex-col h-[250px] w-full lg:max-w-[400px] p-3 border-solid border-2 border-cadet-blue-400  mx-3 rounded-md shadow-lg shadow-cadet-blue-500/70 bg-cadet-blue-200 space-y-3">
       <div className="latest-article-header flex flex-col h-[35%] border-solid border-b-[1px] border-cadet-blue-300">
         <p className="text-lg">{article.title}</p>
-        <p className="text-sm">By {article.author}</p>
+        <p className="text-sm">
+          By {article.author} | In: {article.categoryname}
+        </p>
       </div>
       <div className="latest-article-body flex h-[45%]">
         <p className="text-md">{article.excerpt}</p>
       </div>
       <div className="latest-article-btn-container flex h-[20%] align-middle justify-end">
-        <NavLink to="/articles/">
+        <NavLink to={`/articles/${article.slug}`}>
           <Button variant="ghost">Read more...</Button>
         </NavLink>
       </div>
