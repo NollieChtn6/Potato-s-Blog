560085e48cc1c7b5e2f1a0fec3425751830a9d73
Noëllie Chatain Six <noellie.chatain-six@oclock.school>
2024-06-10 11:19:52 +0200

    remove articlesList const to simplify code

--- src/components/categoriesPage/categoriesPage.tsx
+++ src/components/categoriesPage/categoriesPage.tsx
@@ -3,18 +3,17 @@ import { useCategories } from '../../contexts/categoriesContext';
 
 function Categories() {
   const { categories } = useCategories();
-  const categoriesList = categories;
 
   return (
     <main className="articles-page-container flex flex-col overflow-y-hidden h-full w-full text-pewter-900 p-3 space-y-6">
       <div className="breadcrumbs-container flex bg-slate-400 w-full h-[30px] items-center">
         Page • Page • Page
       </div>
-      <div className="ategories-page-header">
+      <div className="categories-page-header">
         <h2 className="text-4xl font-serif text-pewter-900">Categories</h2>
       </div>
       <div className="categories-page-content flex flex-col w-full overflow-y-scroll space-y-6 lg:flex-row lg:flex-wrap">
-        {categoriesList.map((category) => (
+        {categories.map((category) => (
           <CategoryCard key={category.id} category={category} />
         ))}
       </div>
